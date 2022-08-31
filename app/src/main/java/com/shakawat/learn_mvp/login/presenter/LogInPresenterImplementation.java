bcddb1db505687b07b0aa59b7b43ce6af9a6e1d1
shakawat <shakawat_hossain@outlook.com>
2022-08-31 08:25:49 +0600

    ...

--- app/src/main/java/com/shakawat/learn_mvp/login/presenter/LogInPresenterImplementation.java
+++ app/src/main/java/com/shakawat/learn_mvp/login/presenter/LogInPresenterImplementation.java
@@ -1,5 +1,7 @@
 package com.shakawat.learn_mvp.login.presenter;
 
+import com.shakawat.learn_mvp.login.model.IUser;
+import com.shakawat.learn_mvp.login.model.UserImplementetion;
 import com.shakawat.learn_mvp.login.view.ILoginView;
 
 public class LogInPresenterImplementation implements ILogInPresenter{
@@ -12,6 +14,16 @@ public class LogInPresenterImplementation implements ILogInPresenter{
 
     @Override
     public void doLogin(String email, String pass) {
+        IUser user= new UserImplementetion(email,pass);
+        int login_code= user.checkUserValidity();
+
+        if (login_code==0){
+            loginView.onLoginError("Please Enter your mail:");
+        }
+        if (login_code==1){}
+        if (login_code==2){}
+        if (login_code==3){}
+        if (login_code==-1){}
 
     }
 
